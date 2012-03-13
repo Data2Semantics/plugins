23697722640c777c7aabce9937316f3097b811d1
Rinke Hoekstra <rjhoekstr@gmail.com>
2012-03-13 16:19:47 +0100

    Prefixed urls with 'plugins/'

--- urls.py
+++ urls.py
@@ -6,10 +6,10 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^mascc/(?P<graph>(.|\n)+)$', 'mascc.views.parse'),
-    url(r'^test/(?P<ttype>(.*?))(/(?P<test>(.*)))?$', 'views.test'),
-    url(r'^publications/(?P<graph>(.|\n)+)$', 'publications.views.retrieve'),
-    url(r'^$', 'views.home'),
+    url(r'^plugins/mascc/(?P<graph>(.|\n)+)$', 'mascc.views.parse'),
+    url(r'^plugins/test/(?P<ttype>(.*?))(/(?P<test>(.*)))?$', 'views.test'),
+    url(r'^plugins/publications/(?P<graph>(.|\n)+)$', 'publications.views.retrieve'),
+    url(r'^plugins/$', 'views.home'),
     # url(r'^mascc_plugin/', include('mascc_plugin.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
